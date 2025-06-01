655ca845bfeebfd00b023400cc2b1445bf2b799a
onegood07 <one_good07@naver.com>
2025-06-01 18:09:49 +0900

    feat/#2: 카카오맵에 마커 표시

--- src/components/KaKaoMap.tsx
+++ src/components/KaKaoMap.tsx
@@ -1,7 +1,26 @@
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, CustomOverlayMap, MapMarker } from 'react-kakao-maps-sdk';
+import type { TransformedKakaoPlace } from '../types/kakao';
+import * as S from '../styles/mapPage.style';
+import { BASE_ADDRESS } from '../constants/map.constant';
 
-function KakaoMap() {
-  return <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '1000px', height: '600px' }} level={3} />;
-}
+type KakaoMapProps = {
+  placeData: TransformedKakaoPlace[];
+  center: { lat: number; lng: number };
+  onMarkerClick: (marker: TransformedKakaoPlace) => void;
+};
+
+const KakaoMap: React.FC<KakaoMapProps> = ({ placeData, center, onMarkerClick }) => {
+  return (
+    <Map center={center} style={{ width: '100%', height: '100%' }} level={3}>
+      {placeData.map((marker) => {
+        const lat = typeof marker.y === 'string' ? parseFloat(marker.y) : marker.y;
+        const lng = typeof marker.x === 'string' ? parseFloat(marker.x) : marker.x;
+        return (
+          <MapMarker key={marker.id} position={{ lat, lng }} clickable={true} onClick={() => onMarkerClick(marker)} />
+        );
+      })}
+    </Map>
+  );
+};
 
 export default KakaoMap;
